9efb4d94d97fa3333738b9ace476d9c519e4f596
Ihor <itotonk1@gmail.com>
2022-09-02 19:32:42 +0300

    changes

--- src/fetchPictures.js
+++ src/fetchPictures.js
@@ -1,16 +1,17 @@
 const axios = require('axios').default;
 const BASE_URL = 'https://pixabay.com/api';
-const KEY = '29488143-fc1f5e1ea256bfdc98e4452e8';
+const KEY = '29559865-360b254a5abc6663dbbd46c59';
 
 export async function fetchPicture(clientRequest, page) {
-    try {
-      const images = await axios.get(
-        `${BASE_URL}/?key=${KEY}&q=${clientRequest}&image_type=photo&orientation=horizontal&safesearch=true&page=${page}&per_page=40`);
-        const response = await images.data;
-        return response;
-    } catch (error) {
-      if (error.response) {
-        throw new Error(error.response.status);
-      }
+  try {
+    const images = await axios.get(
+      `${BASE_URL}/?key=${KEY}&q=${clientRequest}&image_type=photo&orientation=horizontal&safesearch=true&page=${page}&per_page=40`
+    );
+    const response = await images.data;
+    return response;
+  } catch (error) {
+    if (error.response) {
+      throw new Error(error.response.status);
     }
   }
+}
